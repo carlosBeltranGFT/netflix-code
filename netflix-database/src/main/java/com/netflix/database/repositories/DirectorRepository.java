c4dafd4286aee60db241c9af7fe94d8c0617c297
josemacias92 <77459050+josemacias92@users.noreply.github.com>
2022-07-28 16:02:21 +0200

    Add files via upload

---
+++ netflix-database/src/main/java/com/netflix/database/repositories/DirectorRepository.java
@@ -0,0 +1,10 @@
+package com.netflix.database.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.netflix.database.entities.Director;
+import com.netflix.database.entities.NetflixTitles;
+
+public interface DirectorRepository extends JpaRepository<Director, Integer>{
+
+}
---
+++ netflix-database/src/test/java/com/netflix/database/NetflixDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
